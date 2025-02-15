f63220e42f83a5b8ef4febae7136f7113eb0b3ba
xihuanbaipiao <149726962+54894334@users.noreply.github.com>
2025-02-16 04:33:03 +0800

    Update qsqd.js

--- qsqd/qsqd.js
+++ qsqd/qsqd.js
@@ -1,4 +1,9 @@
-// auth-capture.js
+
+/*/[rewrite]
++ ^https:\/\/api\.alldragon\.com.* url script-request-header https://raw.githubusercontent.com/54894334/qx/refs/heads/main/qsqd/qsqd.ini
+[mitm]
+hostname = api.alldragon.com
+ auth-capture.js*//
 const key = "nz_qsqd";
 if ($request.headers?.Authorization) {
   const token = $request.headers.Authorization.replace("Bearer ", "");
